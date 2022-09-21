132f34bc44b7b131e5ec14b3c98e3fc138ed5bce
Ghost620 <faghost6201@gmail.com>
2022-09-21 21:58:16 +0500

    icons added

--- whatsapp_mern/src/sidebar.js
+++ whatsapp_mern/src/sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Sidebar.css"
+import ChatIcon from '@mui/icons-material/Chat';
 
 const sidebar = () => {
   return (
@@ -7,7 +8,7 @@ const sidebar = () => {
         sidebar
         <div className='sidebar_header'>
             <div className='sidebar_headerRight'>
-                
+                <ChatIcon />
             </div>
         </div>
     </div>
