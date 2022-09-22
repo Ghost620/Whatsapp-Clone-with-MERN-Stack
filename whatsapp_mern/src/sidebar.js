96a2d8270adbb2c50cee2464ce3765acff3d6be1
Ghost620 <faghost6201@gmail.com>
2022-09-22 15:57:07 +0500

    search bar added

--- whatsapp_mern/src/sidebar.js
+++ whatsapp_mern/src/sidebar.js
@@ -3,20 +3,42 @@ import "./Sidebar.css"
 import ChatIcon from '@mui/icons-material/Chat';
 import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import { Avatar, IconButton } from '@mui/material';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { SearchOutlined } from '@mui/icons-material'
 
 const sidebar = () => {
   return (
     <div className='sidebar'>
-        sidebar
+
         <div className='sidebar_header'>
-            <div className='sidebar_headerRight'>
-                <ChatIcon />
-                <IconButton>
-                  <DonutLargeIcon />
-                </IconButton>
-                
+
+          <Avatar src='https://cdn.pixabay.com/photo/2016/01/31/19/41/apple-1172060_960_720.jpg'/>
+
+          <div className='sidebar_headerRight'>
+              
+            <IconButton>
+              <DonutLargeIcon />
+            </IconButton>
+
+            <IconButton>
+              <ChatIcon />
+            </IconButton>
+
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>            
+              
+          </div>
+
+        </div>
+
+        <div className='sidebar_search'>
+            <div className='sidebar_searchContainer'>
+              <SearchOutlined />
+              <input placeholder='Search or Start new chat' type='text' />
             </div>
         </div>
+        
     </div>
   )
 }
