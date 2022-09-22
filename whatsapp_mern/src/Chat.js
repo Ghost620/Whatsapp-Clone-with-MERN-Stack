9c9620ecc97b653b10a5c7999788b3665e1a0407
Ghost620 <faghost6201@gmail.com>
2022-09-22 23:10:17 +0500

    chat header completed

--- whatsapp_mern/src/Chat.js
+++ whatsapp_mern/src/Chat.js
@@ -1,10 +1,42 @@
 import React from 'react'
 import "./Chat.css"
+import { Avatar, IconButton } from '@mui/material';
+import { SearchOutlined, MoreVert, AttachFile,  } from '@mui/icons-material'
 
 const Chat = () => {
   return (
     <div className='chat'>
-        Chat
+
+        <div className='chat_header'>
+          <Avatar />
+
+          <div className='chat_headerInfo'>
+            <h3>Room Name</h3>
+            <p>Last scene</p>
+          </div>
+
+          <div className='chat_headerRight'>
+
+            <IconButton>
+              <SearchOutlined />
+            </IconButton>
+
+            <IconButton>
+              <AttachFile />
+            </IconButton>
+
+            <IconButton>
+              <MoreVert />
+            </IconButton>
+
+          </div>
+
+        </div>
+
+        <div className='chat_body'>
+          
+        </div>
+
     </div>
   )
 }
