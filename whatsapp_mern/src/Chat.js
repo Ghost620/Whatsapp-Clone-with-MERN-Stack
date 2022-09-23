c5c101a6d03c47021ba807e388dd10862f5c1eed
Ghost620 <faghost6201@gmail.com>
2022-09-23 14:47:30 +0500

    chat footer added

--- whatsapp_mern/src/Chat.js
+++ whatsapp_mern/src/Chat.js
@@ -2,6 +2,8 @@ import React from 'react'
 import "./Chat.css"
 import { Avatar, IconButton } from '@mui/material';
 import { SearchOutlined, MoreVert, AttachFile } from '@mui/icons-material'
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import MicIcon from '@mui/icons-material/Mic';
 
 const Chat = () => {
   return (
@@ -58,6 +60,17 @@ const Chat = () => {
             <span className='chat_timestamp'> {new Date().toUTCString()} </span>
 
           </p>
+
+        </div>
+
+        <div className='chat_footer'>
+
+          <EmojiEmotionsIcon />
+          <form>
+            <input placeholder='Type a message...' type='text'/>
+            <button type='submit'>Send a message</button>
+          </form>
+          <MicIcon />
 
         </div>
 
