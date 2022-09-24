eade893ad6636dda6fdb7502cfa6bbdd62c8a49a
Ghost620 <faghost6201@gmail.com>
2022-09-24 17:57:59 +0500

    .

--- whatsapp_backend/server.js
+++ whatsapp_backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import Messages from "./dbMessages.js"
 
 //app config
 
@@ -10,15 +11,27 @@ const port = process.env.PORT || 9000
 
 const conn_url = 'mongodb+srv://ghost620:Prisoner620123@cluster0.4bcjedd.mongodb.net/whatsappdb?retryWrites=true&w=majority'
 
-mongoose.connect(conn_url,{
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true 
-})
-
+// mongoose.connect(conn_url, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//     useCreateIndex: true 
+// })
+mongoose.connect(conn_url)
 
 // API Routes
 
 app.get('/', (req, res) => res.status(200).send('hello world'))
+
+app.post('/messages/new', (req, res) => {
+    const dbMessage = req.body
+
+    Messages.create(dbMessage, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+})
 
 app.listen(port, ()=> console.log(`Listening on localhost:${port}`))
