a1c988015c2461bc3da40ecdb98b2561d431b3de
Ghost620 <faghost6201@gmail.com>
2022-09-24 16:12:53 +0500

    Update server.js

--- whatsapp_backend/server.js
+++ whatsapp_backend/server.js
@@ -1,7 +1,23 @@
 import express from 'express'
+import mongoose from 'mongoose'
+
+//app config
 
 const app = express()
 const port = process.env.PORT || 9000
+
+// DB config
+
+const conn_url = 'mongodb+srv://ghost620:Prisoner620123@cluster0.4bcjedd.mongodb.net/whatsappdb?retryWrites=true&w=majority'
+
+mongoose.connect(conn_url,{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true 
+})
+
+
+// API Routes
 
 app.get('/', (req, res) => res.status(200).send('hello world'))
 
