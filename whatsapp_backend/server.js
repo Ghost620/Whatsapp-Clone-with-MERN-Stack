66d8f4770ae3ff35a94e8ccaf592c0c1e67c0910
Ghost620 <faghost6201@gmail.com>
2022-09-26 03:18:14 +0500

    cors added

--- whatsapp_backend/server.js
+++ whatsapp_backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import Messages from "./dbMessages.js"
 import Pusher from "pusher"
+import cors from "cors"
 
 // app config
 
@@ -20,13 +21,9 @@ const pusher = new Pusher({
 
 
 // middleware
-app.use(express.json())
 
-app.use( (req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    next();
-})
+app.use(express.json())
+app.use(cors())
 
 // DB config
 
