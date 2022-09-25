a29c806f7cd55faccd25b7cea7b788ef5ed90ecd
Ghost620 <faghost6201@gmail.com>
2022-09-25 14:54:32 +0500

    Update server.js

--- whatsapp_backend/server.js
+++ whatsapp_backend/server.js
@@ -2,27 +2,38 @@ import express from 'express'
 import mongoose from 'mongoose'
 import Messages from "./dbMessages.js"
 
-//app config
+// app config
 
 const app = express()
 const port = process.env.PORT || 9000
 
+// middleware
+app.use(express.json())
+
 // DB config
 
-const conn_url = 'mongodb+srv://ghost620:Prisoner620123@cluster0.4bcjedd.mongodb.net/whatsappdb?retryWrites=true&w=majority'
+const conn_url = 'mongodb+srv://ghost620:Prisoner620123@cluster0.4bcjedd.mongodb.net/?retryWrites=true&w=majority'
 
-// mongoose.connect(conn_url, {
-//     useUnifiedTopology: true,
-//     useNewUrlParser: true,
-//     useCreateIndex: true 
-// })
-mongoose.connect(conn_url, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
-.then(console.log("mongodb connected successfully...."))
-.catch(err =>console.log(err))
+mongoose.connect(conn_url)
+
+// mongoose.connect(conn_url, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+// .then(console.log("mongodb connected successfully...."))
+// .catch(err =>console.log(err))
 
 // API Routes
 
 app.get('/', (req, res) => res.status(200).send('hello world'))
+
+app.get('/messages/sync', (req, res) => {
+
+    Messages.find( (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
+})
 
 app.post('/messages/new', (req, res) => {
     const dbMessage = req.body
