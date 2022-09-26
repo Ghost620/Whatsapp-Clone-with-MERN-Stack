2d1837c48fcb24d7df6b75b9bd8e170550de1a62
Ghost620 <faghost6201@gmail.com>
2022-09-26 15:11:31 +0500

    backend done

--- whatsapp_mern/src/App.js
+++ whatsapp_mern/src/App.js
@@ -1,13 +1,41 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Sidebar from './sidebar';
 import Chat from './Chat';
+import Pusher from "pusher-js";
+import axios from "./axios";
 
 function App() {
+  
+  const [messages, setMessages] = useState([])
 
   useEffect(() => {
-    
+    axios.get('/messages/sync').then(response => {
+      console.log(response.data)
+      setMessages(response.data)
+    })
   }, [])
+
+  useEffect(() => {
+
+    const pusher = new Pusher('f67bd1478faa505418f5', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (newMessage) => {
+      setMessages([...messages, newMessage])
+    });
+
+    // cleanup function to clear listeners
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe()
+    }
+
+  }, [messages])
+
+  console.log(messages)
 
   return (
     <div className="app">
