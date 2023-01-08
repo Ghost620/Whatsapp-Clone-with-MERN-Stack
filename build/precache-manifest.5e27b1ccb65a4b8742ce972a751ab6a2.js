5ee5247c5dacc038645569fbdbb4af68ec8442dc
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2023-01-08 22:40:56 +0500

    deployed

---
+++ build/precache-manifest.5e27b1ccb65a4b8742ce972a751ab6a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "63f7b73d198ebf35dc5f",
+    "url": "/static/css/main.63c141df.chunk.css"
+  },
+  {
+    "revision": "63f7b73d198ebf35dc5f",
+    "url": "/static/js/main.63f7b73d.chunk.js"
+  },
+  {
+    "revision": "77bf7857d533de1d6efe",
+    "url": "/static/js/1.77bf7857.chunk.js"
+  },
+  {
+    "revision": "2827fd7527b089a20de0",
+    "url": "/static/js/2.2827fd75.chunk.js"
+  },
+  {
+    "revision": "dbdbc611e556c891e13a",
+    "url": "/static/js/runtime~main.dbdbc611.js"
+  },
+  {
+    "revision": "9e3a6f0e8c4dd2646e4a69debd24da56",
+    "url": "/index.html"
+  }
+];
