b6635110bdc18262dda3395095fa42b677367087
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2023-01-08 22:16:53 +0500

    backend deployed

--- whatsapp_mern/src/axios.js
+++ whatsapp_mern/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9000'
+    baseURL: 'https://whatsappclone.mhuzaifa4.repl.co'
 })
 
 export default instance
